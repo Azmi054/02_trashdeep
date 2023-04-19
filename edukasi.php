554c904bb33f44dffa942b8677ac42315db3b8a7
Nasywa <nasywa44@mhs.ac.id>
2023-04-19 22:03:25 +0700

    halaman edukasi

--- edukasi.php
+++ edukasi.php
@@ -9,57 +9,74 @@
       href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@200&family=Montserrat:wght@400;500;600;700;800&display=swap"
       rel="stylesheet"
     />
-    <title>Trashdeep</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"
+    />
+    <title>Trasndeep</title>
     <link rel="icon" href="assets/logo.png" type="image/x-icon" />
+    <link rel="stylesheet" href="style/edukasi.css" />
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
       rel="stylesheet"
       integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
       crossorigin="anonymous"
     />
-    <link rel="stylesheet" href="style/edukasi.css" />
   </head>
 
   <body>
     <nav>
       <div class="navbar navbar-inversen">
-        <a class="navbar-brand" href="home.php">
-          <img
-            src="assets/beranda.svg"
-            draggable="false"
-            alt="Trashdeep"
-            height="40"
-          />
-          Beranda
-        </a>
-      </div>
+          <a class="navbar-brand" href="home.php">
+            <img
+              src="assets/beranda.svg"
+              draggable="false"
+              alt="Trashdeep"
+              height="40"
+            />
+            Beranda
+          </a>
+        </div>
     </nav>
-
-
-    <footer>
-    <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">Judul Informasi</h5>
-      <p class="card-text">Isi informasi yang ingin ditampilkan</p>
-      <a href="#" class="btn btn-primary">Baca Selengkapnya</a>
+    <div class="container m-3">
+        <h1>Yuk ikuti edukasi tentang 
+          bagaimana penanganan sampah!</h1>
+      </div>
     </div>
-</div>
-<div class="card">
-    <div class="card-body">
-      <h5 class="card-title">Judul Informasi</h5>
-      <p class="card-text">Isi informasi yang ingin ditampilkan</p>
-      <a href="#" class="btn btn-primary">Baca Selengkapnya</a>
-    </div>
-</div>
-    </footer>
+    
 
-
+    <div class="container mt-3">
+      <div class="row">
+        <div class="col mb-4">
+            <div class="card">
+              <h5 class="card-title m-3 ">Tips & Tricks</h5>
+              <div class="row no-gutters">
+                <div class="p-4 text-end">
+                  <a href="#" class="btn btn-primary">Baca Selengkapnya</a>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col mb-4">
+            <div class="card ">
+              <h5 class="card-title m-3">Tutorial</h5>
+              <div class="row no-gutters">
+                <div class="p-4 text-end">
+                  <a href="#" class="btn btn-primary">Baca Selengkapnya</a>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
 
     <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
       crossorigin="anonymous"
     ></script>
-    <script src="script.js"></script>
   </body>
 </html>
