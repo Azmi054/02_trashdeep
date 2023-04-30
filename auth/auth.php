d62a09bc2b1ff193af639f097bf302e9432cc33b
Azmi054 <azmi99@mhs.unsyiah.ac.id>
2023-04-30 16:40:11 +0700

    Ubah tampilan

--- auth/auth.php
+++ auth/auth.php
@@ -1,16 +1,27 @@
 <?php
-include 'koneksi.php'; 
+include 'koneksi.php';
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+if(isset($_POST['no_telepon']) && isset($_POST['password'])) {
+    $no_telepon = mysqli_real_escape_string($conn, $_POST['no_telepon']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-$query = mysqli_query($conn,"SELECT * from tb_masyarakat where username = '$username' and password = '$password'");
-$result = mysqli_num_rows($query);
+    $query = "SELECT * FROM tb_masyarakat WHERE no_telepon = ? AND password = ?";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "ss", $no_telepon, $password);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
 
-if($result > 0 ){
-    header("location: ../home.php");
-}
-else{
-    header("location:../login.php?pesan=error");
-}
+    if(mysqli_num_rows($result) == 1) {
+        // Jika kredensial sesuai, arahkan ke halaman utama
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['no_telepon'] = $row['no_telepon'];
+        header("location: ../Home/home.php");
+        exit();
+    }
+    else {
+        // Jika kredensial tidak sesuai, arahkan kembali ke halaman login dengan pesan error
+        header("location:../Login/login.php?pesan=error&login_error=username_password_salah");
+        exit();
+    }
+} 
 ?>
