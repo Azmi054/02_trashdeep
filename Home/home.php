34005b86e2ca8c81beba18fb40492b63833d4bbc
AufaRz <aufarizki@mhs.unsyiah.ac.id>
2023-05-04 23:15:43 +0700

    Halaman Edukasi

--- Home/home.php
+++ Home/home.php
@@ -30,7 +30,7 @@ session_start();
 
   <body class="body">
     <nav class="navbar navbar-home">
-      <img src="../assets/name.svg" draggable="false" alt="Trashdeep" height="50">
+      <img src="../assets/name.svg" draggable="false" alt="Trashdeep" height="40">
         <div class="container">
           <a class="navbar-brand" href="../Profil/profil.php">
                 <div class="d-flex align-items-center">
@@ -71,7 +71,7 @@ session_start();
         </div>
       <div class="row">
         <div class="col mb-4">
-        <a href="edukasi.php" style ="text-decoration :none">
+        <a href="../Edukasi/edukasi.php" style ="text-decoration :none">
           <div class="card">
             <h5 class="card-title m-3">Edukasi</h5>
           </div>
